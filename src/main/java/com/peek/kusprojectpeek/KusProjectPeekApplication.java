289abc392b16aa1bd0c3a57a77c8c40057462ffe
minji6 <pullzee6@gmail.com>
2024-03-24 03:10:25 +0900

    #2 - 스프링 부트 프로젝트 시작
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/com/peek/kusprojectpeek/KusProjectPeekApplication.java
@@ -0,0 +1,13 @@
+package com.peek.kusprojectpeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KusProjectPeekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KusProjectPeekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/peek/kusprojectpeek/KusProjectPeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peek.kusprojectpeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KusProjectPeekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
