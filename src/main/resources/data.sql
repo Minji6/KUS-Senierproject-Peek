66ef5d44497150bbcfda3a984481ada69ceb0382
나강민 <139530542+KangminNa@users.noreply.github.com>
2024-03-25 18:42:27 +0900

    Feat : db 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+
+-- 초기 데이터 삽입
+INSERT INTO users (username, email) VALUES ('user1', 'user1@example.com');
+INSERT INTO users (username, email) VALUES ('user2', 'user2@example.com');
+INSERT INTO users (username, email) VALUES ('user3', 'user3@example.com');
