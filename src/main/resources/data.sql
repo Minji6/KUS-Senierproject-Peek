6ff4b662f7adda2a90e99fa5145346d93b42ba67
Minji6 <pullzee6@gmail.com>
2024-04-04 03:27:34 +0900

    내용 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,8 @@
-
+CREATE TABLE users (
+                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                       username VARCHAR(50) NOT NULL,
+                       email VARCHAR(100) NOT NULL
+);
 
 -- 초기 데이터 삽입
 INSERT INTO users (username, email) VALUES ('user1', 'user1@example.com');
