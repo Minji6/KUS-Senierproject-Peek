dc0733943c15762fd46250cb2517990854fdc8bf
Minji6 <pullzee6@gmail.com>
2024-04-08 03:18:58 +0900

    스프링 시큐리티 버전 확인 후 수정

---
+++ src/main/java/com/peek/kusprojectpeek/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.peek.kusprojectpeek.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+@Configurable
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableWebMvc
+public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin((formLogin)-> {
+                    formLogin.loginPage("/login");
+                })
+                .build();
+    }
+
+}
